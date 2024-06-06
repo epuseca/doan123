dfe1f582e1649ccd549c44839a6fc35d62e07639
epuseca <nguyengialoc240101@gmail.com>
2024-06-06 16:27:43 +0700

    them env

--- src/config/database.js
+++ src/config/database.js
@@ -24,7 +24,7 @@ const connection = async () => {
     const options = {
       user: process.env.DB_USER,
       pass: process.env.DB_PASSWORD,
-      dbName:process.env.DB_NAME
+      dbName: process.env.DB_NAME
     }
     await mongoose.connect(process.env.DB_HOST, options);
     const state = Number(mongoose.connection.readyState);
